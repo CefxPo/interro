5c1dfa6375c7c118804e9f5c2da712b2c268dc71
Marie Renard <marie.renard@cesjb.be>
2025-11-20 15:57:53 +0100

    Je t'en prie, MARCHEEEEEEEEEEEEEEE!!!!!!!!!

---
+++ main.js
@@ -0,0 +1,4 @@
+import Etudiant from Etudiant.js
+var etudiant1 = new Etudiant("Riri",14,"Latin")
+var etudiant2 = new Etudiant("Fifi",187,"Histoire-Math")
+var etudiant3 = new Etudiant("Loulou",9,"Math-SciencesEco")
